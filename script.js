19cc247e264c69b82cccba2061ddd32e06764cfc
cholekcoche <cholekcoche@gmail.com>
2025-12-12 19:03:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const btn = document.getElementById("especialidades")
+const CLeche = document.getElementById("CLeche")
+const CCaramelo = document.getElementById("CCaramelo")
+const CPistacho = document.getElementById("CPistacho")
+
+
+btn.addEventListener("click", () => {
+
+    if (CLeche.classList.contains("Bajar")) {
+        CLeche.classList.remove("Bajar")
+        CLeche.style.opacity = 1
+        CLeche.style.animationDelay = "0s"
+        CLeche.classList.add("Subir")
+
+        CCaramelo.classList.remove("Bajar")
+        CCaramelo.style.opacity = 1
+        CCaramelo.classList.add("Subir")
+
+        CPistacho.classList.remove("Bajar")
+        CPistacho.style.opacity = 1
+        CPistacho.style.animationDelay = "2.5s"
+        CPistacho.classList.add("Subir")
+    }
+
+    else{
+        CLeche.classList.remove("Subir")
+        CLeche.style.opacity = 0
+        CLeche.style.animationDelay = "2.5s"
+        CLeche.classList.add("Bajar")
+
+        CCaramelo.classList.remove("Subir")
+        CCaramelo.style.opacity = 0
+        CCaramelo.classList.add("Bajar")
+
+        CPistacho.classList.remove("Subir")
+        CPistacho.style.opacity = 0
+        CPistacho.style.animationDelay = "0s"
+        CPistacho.classList.add("Bajar")
+
+
+    }
+
+
+
+})
