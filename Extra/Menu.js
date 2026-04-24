0f573c37c1713a6da9e0fb3508982a37db3dff80
cholekcoche <cholekcoche@gmail.com>
2026-04-24 13:19:52 +0200

    Delete Extra directory

--- Extra/Menu.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btnAnimar");
-const caja = document.getElementById("caja");
-
-btn.addEventListener("click", () => {
-  caja.classList.remove("animar"); // reinicia la animación
-  void caja.offsetWidth;           // truco para reiniciar correctamente
-  caja.classList.add("animar");    // activa la animación
-});
